9b3775d066110db40fb98641b182371274a53e60
Will Fuqua <wafuqua@gmail.com>
2014-09-07 14:36:39 +0700

    turn responses with newlines into separate msgs

--- src/cleric/responses.clj
+++ src/cleric/responses.clj
@@ -1,7 +1,8 @@
 (ns cleric.responses
   "IRC response system. To make the bot respond to a 
   specific IRC command, implement the corresponding multimethod"
-  (:require [cleric.irc :as irc] 
+  (:require [clojure.string :refer [split-lines blank?]]
+            [cleric.irc :as irc] 
             [cleric.plugins :as plugins]))
 
 (defmulti response :command)
@@ -15,8 +16,10 @@
   :privmsg [{[target] :params 
              text :trailing
              :as request}]
-  (if (not= target "cleric") ;don't response to PMs
+  (if (not= target "cleric") ;don't respond to PMs
     (let [responses (plugins/run text)
+          message-text (filter (complement blank?) 
+                               (mapcat split-lines responses))
           create-privmsg (partial irc/privmsg request)]
-      (map create-privmsg responses))))
+      (map create-privmsg message-text))))
 
